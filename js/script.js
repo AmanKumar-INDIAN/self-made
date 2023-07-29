71165de048f0c2395e101ac1b6b7de7781fbada3
Aman bhai <rohanrikwar9555@gmail.com>
2023-07-29 16:15:51 +0530

    projett

--- js/script.js
+++ js/script.js
@@ -27,6 +27,37 @@ if(sessionStorage.getItem("theme")){
 const changetheme=()=>{
     $html.dataset.theme= sessionStorage.getItem("theme")==="light" ? "dark":"light";
 sessionStorage.setItem("theme",$html.dataset.theme);
+console.log("button")
 }
 
 $themeBtn.addEventListener("click",changetheme)
+
+
+/***
+ * TAB
+ */
+
+const $tabBtn=document.querySelectorAll("[data-tab-btn]");
+let [lastActiveTab]=document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn]=$tabBtn;
+
+console.log(lastActiveTab)
+console.log(lastActiveTabBtn)
+$tabBtn.forEach(item =>{
+    item.addEventListener("click",function(){
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+console.log(item.dataset)
+        const $tabContent=document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+       $tabContent.classList.add("active");
+console.log($tabContent)
+       this.classList.add("active");
+       console.log(this)
+
+       lastActiveTab=$tabContent;
+       lastActiveTabBtn=this;
+
+
+
+    })
+})
